e6d37852fdc0a5142bfa1e8e8ce1821d7619a58e
saikrishna <saikrishna31120@gmail.com>
2023-11-08 13:54:04 +0530

    included pybind11 module

--- lemon-a278d16bd2d0/lemon/hao_orlin.h
+++ lemon-a278d16bd2d0/lemon/hao_orlin.h
@@ -26,6 +26,7 @@
 #include <lemon/maps.h>
 #include <lemon/core.h>
 #include <lemon/tolerance.h>
+#include <pybind11/pybind11.h>
 
 /// \file
 /// \ingroup min_cut
@@ -1011,5 +1012,31 @@ namespace lemon {
   }; //class HaoOrlin
 
 } //namespace lemon
+
+namespace py = pybind11;
+
+//const Digraph& graph, const CapacityMap& capacity, const Tolerance& tolerance = Tolerance()
+PYBIND11_MODULE(lemon_hao_orlin, m) {
+    m.doc() = R"pbdoc(
+        Hao orlin algorithm for unrestricted minimum cut
+
+        .. currentmodule:: lemon_hao_orlin
+
+        .. autosummary::
+           :toctree: _generate
+          
+          class HaoOrlin 
+
+           
+    )pbdoc";
+
+    py::class_<HaoOrlin>(m, "HaoOrlin")
+        .def(py::init<const lemon::Digraph &, const lemon::CapacityMap &, const lemon::Tolerance &>(),py::arg("tolerance")=lemon::Tolerance())
+        .def("init", &HaoOrlin::init)
+        .def("calculateOut", &HaoOrlin::calculateOut);
+        .def("minCutValue", &HaoOrlin::minCutValue);
+        .def("minCutMap", &HaoOrlin::minCutMap);
+}
+
 
 #endif //LEMON_HAO_ORLIN_H
